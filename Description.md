f067eab71684e288cc4113c4a36a9aa357bf0ffd
Marco Camarena <mcospino@gmail.com>
2021-08-10 21:01:35 +0200

    Fixed the plotting

--- Description.md
+++ Description.md
@@ -1,8 +1,10 @@
 Flight Mapper
 
 [x] Why is "csv cols" bugging out!
-  # the line above was written as (color = #someting) making the parenthesis open
+  # the line above was written as (color = #something) making the parenthesis open
 [x] Try to add all coordinate data to a series
   # deleted the for-loop and left the ax.plot(routes['coordinates'])
-[ ] The code work but now the lines are not plotted properly -FIX
+[x] The code work but now the lines are not plotted properly -FIX
+  # Managed to fix the lines buy putting the lats and lons together in one plot
+[ ] Find how to make lines starter point to be DXB
 [ ] Fix other minor any bugs
--- mapper_copy.py
+++ mapper_copy.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
 def main(filename):
+    #Float is size in inches
+    fig = plt.figure(figsize = (30.0, 30.0))
 
-    fig = plt.figure(figsize = (30.0, 20.0))
     # add_subplot(nrows, ncols, index, **kwargs)
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+
     # make the map global
     ax.set_global()
 
+    # changed color to subtle pink
     ax.coastlines(color = '#ff6188')
 
     # define the expected CSV columns
@@ -21,17 +24,13 @@ def main(filename):
     #read the csv
     routes = pd.read_csv(filename, names = csv_cols, na_values=['\\N'], sep = ',', skiprows = 1)
 
-    #for i, route in enumarate(routes.sort_values(by = 'flight_nb', ascending = True).iterrows()):
+    # dep and arr aggregate in two plots
+    # line color changed to cyan
+    # Geodetic projection gives the curve to the line
+    ax.plot(routes['dep_lat'], routes['dep_lon'], color = '#78dce8', transform=ccrs.Geodetic())
+    ax.plot(routes['arr_lat'], routes['arr_lon'], color = '#78dce8',transform=ccrs.Geodetic())
 
-        #route = route[1]
-
-        # need to rewrap column in Series to apply key function
-    ax.plot(routes['dep_lat'], transform=ccrs.PlateCarree())
-    ax.plot(routes['dep_lon'], transform=ccrs.PlateCarree())
-    ax.plot(routes['arr_lat'], transform=ccrs.PlateCarree())
-    ax.plot(routes['arr_lon'], transform=ccrs.PlateCarree())
-
-    # save the map
+    #display the map
     plt.show()
 
 # to print the map
