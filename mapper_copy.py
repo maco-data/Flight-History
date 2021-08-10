a4b39504aae33c6726f6d0d1fb8adddc3119fc0f
Marco Camarena <mcospino@gmail.com>
2021-08-10 17:39:24 +0200

    Fixed two big bugs

--- mapper_copy.py
+++ mapper_copy.py
@@ -7,33 +7,33 @@ import cartopy.crs as ccrs
 
 def main(filename):
 
-    fig = plt.figure(figsize = (27.0, 20.0))
+    fig = plt.figure(figsize = (30.0, 20.0))
+    # add_subplot(nrows, ncols, index, **kwargs)
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-
     # make the map global
     ax.set_global()
 
-    # Make the next line available to display the "earth" satellite immage
-    # ax.stock_img()
-    ax.coastlines(color = #ff6188)
+    ax.coastlines(color = '#ff6188')
 
     # define the expected CSV columns
-    CSV_COLS = ('dep_lat', 'dep_lon', 'arr_lat', 'arr_lon', 'flight_nb')
+    csv_cols = ('dep_lat', 'dep_lon', 'arr_lat', 'arr_lon', 'flight_nb')
 
     #read the csv
-    routes = pd.read_csv(filename, names = CSV_COLS, na_values=['\\N'],
-                        sep = ',', skiprows = 1)
+    routes = pd.read_csv(filename, names = csv_cols, na_values=['\\N'], sep = ',', skiprows = 1)
 
-    for i, route in enumarate(routes.sort_values(by = 'flight_nb', ascending = True).iterrows()):
+    #for i, route in enumarate(routes.sort_values(by = 'flight_nb', ascending = True).iterrows()):
 
-        route = route[1]
+        #route = route[1]
 
         # need to rewrap column in Series to apply key function
-        ax.plot(routes['dep_lat']), routes['dep_lon']), routes['arr_lat']), routes['arr_lon']),
+    ax.plot(routes['dep_lat'], transform=ccrs.PlateCarree())
+    ax.plot(routes['dep_lon'], transform=ccrs.PlateCarree())
+    ax.plot(routes['arr_lat'], transform=ccrs.PlateCarree())
+    ax.plot(routes['arr_lon'], transform=ccrs.PlateCarree())
 
     # save the map
     plt.show()
 
+# to print the map
 if __name__ == '__main__':
     main('flight_map.csv')
-# to print the map
